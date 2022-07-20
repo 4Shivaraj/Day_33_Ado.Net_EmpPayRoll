e0f367c826c6429916aedf7479ed19d6835a75b5
4Shivaraj <4shivaraj.gowda@gmail.com>
2022-07-20 15:09:18 +0530

    [Shivaraj] Added method to add data into database

--- ADOnetProblems/ADOnetProblems/Program.cs
+++ ADOnetProblems/ADOnetProblems/Program.cs
@@ -10,24 +10,55 @@ namespace EmployeePayrollSQL
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome");
+
             EmployeeRepo repo = new EmployeeRepo();
-            repo.GetAllEmployees();
-            Console.ReadLine();
+            try
+            {
+                Console.WriteLine("Choose option or press 0 for exit\n1:Retrieve Data\n2:Add Data\n");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 0:
+                        Environment.Exit(0);
+                        break;
+                    case 1:
+                        repo.GetAllEmployees();
+                        break;
+                    case 2:
+                        EmployeeModel model = new EmployeeModel
+                        {
+                            Name = "Pant",
+                            Startdate = DateTime.Now,
+                            Gender = 'M',
+                            Phone = 912423,
+                            Department = "DC",
+                            Address = "Delhi",
+                            Basic_Pay = 30000.00,
+                            Deductions = 1000.00,
+                            Taxable_Pay = 29000.00,
+                            Income_Tax = 1000.00,
+                            Net_Pay = 28000,
+                        };
+                        repo.AddEmployee(model);
+                        break;
+                }
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
 
-
 /*
-1, Dhoni, 70000, 03 - 02 - 2007 00:00:00, M, 1234567890, Bangalore, , 0, 0, 0, 70000
+Welcome
+Choose option or press 0 for exit
+1:Retrieve Data
+2:Add Data
 
-2, Virat, 50000, 04 - 05 - 2010 00:00:00, M, 1234569874, Bangalore, , 0, 0, 0, 50000
-
-3, Rohit, 60000, 09 - 06 - 2009 00:00:00, M, 1234567890, Bangalore, , 0, 0, 0, 60000
-
-4, Smrithi, 40000, 05 - 03 - 2018 00:00:00, F, 1234569874, Bangalore, , 0, 0, 0, 40000
-
-5, Mithai, 50000, 02 - 08 - 2020 00:00:00, F, 1234567890, Bangalore, , 0, 0, 0, 50000
-
-6, Virat, 60000, 05 - 05 - 2011 00:00:00, M, 7894561230, Bangalore, RCB, 1000, 59000, 2000, -2000
+2
+Employee details added successfully
 */
